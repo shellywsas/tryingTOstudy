45d568f4fe0e6e48ee3dd28aee77575ad5661843
shellywsas <shelly.wsas@gmail.com>
2026-09-15 00:00:27 +0300

    feat: admin god mode with double penalty refund, weekly crowning, parent whatsapp improvements, and strict subject task copying

--- sw.js
+++ sw.js
@@ -9,11 +9,9 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('activate', (event) => {
     event.waitUntil(
-        caches.keys().then((keys) => {
-            return Promise.all(
-                keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-            );
-        }).then(() => self.clients.claim())
+        caches.keys().then(keys => Promise.all(
+            keys.map(key => caches.delete(key))
+        )).then(() => self.clients.claim())
     );
 });
 
